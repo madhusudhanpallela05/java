e689f766abc948494da521ef961cdbba006b6195
madhusudhanpallela05 <151707585+madhusudhanpallela05@users.noreply.github.com>
2024-08-22 12:34:58 +0530

    Create Switch_demo.java

---
+++ Switch_demo.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+public class Switch_demo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number 0-5");
+        int num=sc.nextInt();
+        switch(num){
+            case 0:
+                System.out.println("You entered zero");
+                break;
+            case 1:
+                System.out.println("You entered one");
+                break;
+            case 2:
+                System.out.println("You entered two");
+                break; 
+            case 3:
+                System.out.println("You entered three");
+                break;
+            case 4:
+                System.out.println("You entered four");
+                break;
+             case 5:
+                System.out.println("You entered five");
+                break;
+            default:
+            System.out.println("You entered invalid no");
+            
+        }
+    }
+}
