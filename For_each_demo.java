dc9a34a0110e1bc7cc4b69bd3fab6b74eedc2162
madhusudhanpallela05 <151707585+madhusudhanpallela05@users.noreply.github.com>
2024-09-05 11:27:20 +0530

    Create For_each_demo.java

---
+++ For_each_demo.java
@@ -0,0 +1,14 @@
+// to print the elemrnts in array using for each loop
+import java.io.*;
+public class For_each_demo{
+    public static void main(String args[])
+    {
+        int arr[]={10,20,30,40,50};
+        System.out.println("The array elements are:");
+        for(int i=0;i<5;i++)//i<arr.lenght 
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
